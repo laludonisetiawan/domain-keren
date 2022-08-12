b9af44dfc2649f27fde2e0e1407d67ca8349139a
laludonisetiawan <laludonisetiawan@gmail.com>
2022-08-12 16:53:50 +0800

    modiefed file domain.sol and run.js

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,20 +5,33 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
-    // Tipe data "pemetaan" untuk menyimpan nama mereka
-    mapping (string => address) public domains;
+    mapping (string => address) public domains; // Tipe data "pemetaan" untuk menyimpan nama mereka
+    mapping (string => string) public records; // mapping untuk menyimpan nilai 
+    
     constructor() {
         console.log("halo bang ini adalah contract untuk domain saya, keren bukan? bukan!");
     }
 
     // Fungsi register yang menambahkan namanya ke Mapping kita
     function register (string calldata name) public {
+        require(domains[name] == address(0)); // periksa apakah namanya tidak terdaftar, jadi jika domain belum d terdaftar akan merujuk ke angka nol. 
         domains[name] = msg.sender;
         console.log("%s telah mendaftarkan domain!", msg.sender);
     }
 
     // Ini akan memberi kami alamat pemilik domain
     function getAddress(string calldata name) public view returns (address) {
-        return domains[name];
+        return domains[name]; // cek apakah pengirim transaksi adalah alamat yang memiliki domain tersebut.
+    }
+
+    // fungsi getRecord untuk memberikan nilai 
+    function setRecord(string calldata name, string calldata record) public  {
+        // periksa apakah pemiliknya yang mengirimkan transaksi 
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name) public view returns(string memory) {
+        return records[name];
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,16 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
-    console.log("Contract deployed to:", domainContract.address);
+    console.log("Contract disebarkan ke :", domainContract.address);
+    console.log("Contract disebarkan oleh:", owner.address); //untuk melihat orang yang menyebarkan kontrak kami
+
+    let txn = await domainContract.register("doom");
+    await txn.wait();
+
+    const domainOwner = await domainContract.getAddress("doom");
+    console.log("Pemilik domain:", domainOwner);
   };
   
   const runMain = async () => {
