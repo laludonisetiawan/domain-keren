a5fd745c6efed09bf59e67a53005a169916b443c
laludonisetiawan <laludonisetiawan@gmail.com>
2022-08-14 02:52:58 +0800

    error saat menjual domain sebagai NFT, revisi besok

--- scripts/run.js
+++ scripts/run.js
@@ -1,31 +1,30 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners(); // Pengembalian pertama adalah penyebar, yang kedua adalah akun acak
-    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
-    await domainContract.deployed();
-    console.log("Contract disebarkan ke :", domainContract.address);
-    console.log("Contract disebarkan oleh:", owner.address); //untuk melihat orang yang menyebarkan kontrak kami
+  const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+  // We pass in "ninja" to the constructor when deploying
+  const domainContract = await domainContractFactory.deploy("ninja");
+  await domainContract.deployed();
 
-    let txn = await domainContract.register("doom");
-    await txn.wait();
+  console.log("Contract deployed to:", domainContract.address);
 
-    const domainAddress = await domainContract.getAddress("doom");
-    console.log("Pemilik domain doom:", domainAddress);
-
-
-  // Mencoba membuat rekor yang bukan milikku!
-  tnx = await domainContract.connect(randomPerson).setRecord("doom", "Haha domain saya sekarang!");
+  // We're passing in a second variable - value. This is the moneyyyyyyyyyy
+  let txn = await domainContract.register("mortal",  {value: hre.ethers.utils.parseEther('0.1')});
   await txn.wait();
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+
+  const address = await domainContract.getAddress("mortal");
+  console.log("Owner of domain mortal:", address);
+
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
